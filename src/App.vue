40a0908d528e566ecedd1960424399d170f1780b
fatemenouri <fatemenouri99@gmail.com>
2022-04-19 15:40:44 +0430

    add global style file

--- src/App.vue
+++ src/App.vue
@@ -31,11 +31,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 60px;
 }
 
 .title {
-  color: #2c3e50;
+  color: #ffffff;
 }
 </style>
